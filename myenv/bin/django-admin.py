675ec31335a70018664e5e0ced70f613f57d884a
Philip Van Kampen <plvankampen@gmail.com>
2018-08-19 23:36:40 -0400

    init commit for django website tutorial

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/philip/Programming/web_design/django_site_tutorial/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
